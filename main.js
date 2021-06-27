d28c1e10aac4bac02cbb86dbfb6a8ef3d5e66feb
1234Health24 <86455393+1234Health24@users.noreply.github.com>
2021-06-27 12:40:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 verified = 'false'
+verifiedapp ='no'
 rdmnmbr = Math.floor(100000 + Math.random() * 900000)
-
+// when we get a message from the app, display it on the page
+ThunkableWebviewerExtension.receiveMessage(function(message) {
+  // Do something with your message
+  if(message == 'App Downloaded')
+  {verifiedapp = 'yes'}
+});
 ThunkableWebviewerExtension.postMessage(rdmnmbr)
-
+alert(rdmnmbr)
 function Checkforcode() {
 
   userincode = document.getElementById('code').value
   if (userincode == rdmnmbr) {
+    verified = 'yes'
     window.open("videocll.html");
     document.getElementById('result').innerHTML = 'Logging you in'
-    verified = 'true'
+    verified = 'yes'
+    console.log(verified)
   }
   else
     document.getElementById('result').innerHTML = 'Code Invalid'
@@ -18,9 +26,9 @@ function Checkforcode() {
 }
 function checkifverified() {
 
-
   if (verified == "false") {
-
-    window.open("index.html", "_self");
+    if(verifiedapp =='no')
+     console.log(verified)
+    window.open("index.html" , "_self");
   }
 }
